7616a01aafcfe95546996ff5141868493c15727f
Dimitry Machado Marinho <65462657+dimitrymm@users.noreply.github.com>
2024-07-17 12:37:22 -0300

    Finalização API
    criação do CRUD

--- src/routes.js
+++ src/routes.js
@@ -11,5 +11,20 @@ module.exports = [
         method:'GET',
         handler: UserController.getUserById,
     },  
+    {
+        endpoint: '/users',
+        method:'POST',
+        handler: UserController.createUser,
+    }, 
+    {
+        endpoint: '/users/:id',
+        method:'PUT',
+        handler: UserController.updateUser,
+    }, 
+    {
+        endpoint: '/users/:id',
+        method:'DELETE',
+        handler: UserController.deleteUser,
+    }, 
     
 ]
